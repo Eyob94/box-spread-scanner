dce862d212cc29494c9a605baf0b17fef1313314
Eyob <eyobmalik@gmail.com>
2026-09-07 13:34:56 +0400

    Add env to config

--- src/config.rs
+++ src/config.rs
@@ -2,12 +2,12 @@ use clap::Parser;
 
 #[derive(Debug, Clone, Parser)]
 pub struct Config {
-    #[arg(short, long, default_value = "4001")]
+    #[arg(short, long, default_value = "4001", env = "IB_PORT")]
     pub ib_port: u16,
 
-    #[arg(short, long, default_value = "5")]
+    #[arg(short, long, default_value = "5", env = "CLIENT_ID")]
     pub client_id: u16,
 
-    #[arg(short, long, default_value = "4045")]
+    #[arg(short, long, default_value = "4045", env = "PORT")]
     pub server_port: u16,
 }
